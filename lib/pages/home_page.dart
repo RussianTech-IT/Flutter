8046e8f2fc321955db2b83e979a7013f625f39e3
FloyzenCode <vasyplay323@gmail.com>
2023-06-27 02:08:34 +0400

    Update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,9 +12,14 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.deepPurpleAccent,
+        bottomNavigationBar: BottomNavigationBar(items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.book), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.info), label: ''),
+        ]),
         body: SafeArea(
             child: Padding(
-          padding: const EdgeInsets.all(25.0),
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
           child: Column(
             children: [
               Row(
@@ -25,27 +30,72 @@ class _HomePageState extends State<HomePage> {
                     "Russian Tech",
                     style: TextStyle(
                         color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold
-                    ),
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
                   ),
                   Container(
                     decoration: BoxDecoration(
-                      color: Colors.black38,
-                      borderRadius: BorderRadius.circular(12)
-                      ),
+                        color: Colors.black38,
+                        borderRadius: BorderRadius.circular(12)),
                     padding: EdgeInsets.all(12),
                     child: Icon(
                       Icons.notifications,
                       color: Colors.white,
-                      ),
+                    ),
                   ),
                 ],
-              )
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              // search
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white38,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                padding: EdgeInsets.all(12),
+                child: Row(children: [
+                  Icon(
+                    Icons.search,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text('Поиск'),
+                ]),
+              ),
+
+              SizedBox(
+                height: 25,
+              ),
+
+              // Text Language Programming like
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Выберите\nязык программирования",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 19,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+
+              SizedBox(
+                height: 25,
+              ),
+
+              Expanded(
+                child: Container(
+                  color: Colors.white,
+                ),
+              ),
             ],
           ),
-        )
-      )
-    );
+        )));
   }
 }
