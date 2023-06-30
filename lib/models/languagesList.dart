978871b88e657a9753367f55ee8e78268ca5a7b2
FloyzenCode <vasyplay323@gmail.com>
2023-07-01 02:30:16 +0400

    Add new elements list

--- lib/models/languagesList.dart
+++ lib/models/languagesList.dart
@@ -8,7 +8,12 @@ class LanguagesList {
     {'name': 'C++', 'icon': Icons.code},
     {'name': 'Swift', 'icon': Icons.code},
     {'name': 'Ruby', 'icon': Icons.code},
+    {'name': 'C#', 'icon': Icons.code}, // Add: C#
+    {'name': 'Dart', 'icon': Icons.code}, // Add: Dart
+    {'name': 'Kotlin', 'icon': Icons.code}, // Add: Kotlin
+    {'name': 'Vue.js', 'icon': Icons.code}, // Add: Vue.js
+    {'name': 'React', 'icon': Icons.code}, // Add: React
   ];
-
+  
   List<Map<String, dynamic>> get _languages => languages;
 }
