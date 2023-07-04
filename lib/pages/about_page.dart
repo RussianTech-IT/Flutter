0938b2587ce35bf2d3a0b4aa33caec62f2d45542
FloyzenCode <vasyplay323@gmail.com>
2023-07-05 01:13:21 +0400

    Update about page for test navigation

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -1,13 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:rutech_mobile/pages/home_page.dart';
 
-class AboutPage extends StatelessWidget {
+class AboutPage extends StatefulWidget {
   const AboutPage({super.key});
+
+  @override
+  State<AboutPage> createState() => _AboutPageState();
+}
+
+class _AboutPageState extends State<AboutPage> {
+  String resTest = "";
+  // bottom el
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Books',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: About',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    // for test
+
+    if (index == 0) {
+      resTest = "Home";
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => HomePage()));
+    } else if (index == 1) {
+      resTest = "Books";
+    } else if (index == 2) {
+      resTest = "About";
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => AboutPage()));
+    }
+    print('Clicked Bottom Navigator ${resTest} element ✅');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
-      body: Text('ABOUT!'),
+      body: Center(
+        child: Text('About Page'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.book), label: 'Books'),
+          BottomNavigationBarItem(icon: Icon(Icons.info), label: 'About'),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.black,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
